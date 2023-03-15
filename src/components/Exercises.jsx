bd77ccbb78c3b40b31c3cedde9448a157b3b8077
Hoan <hoanleecr@gmail.com>
2023-03-15 16:08:11 -0700

    finish exercise card

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -6,7 +6,31 @@ import ExerciseCard from "./ExerciseCard";
 import { ExerciseOptions, fetchData } from "../utils/fetchData";
 
 function Exercises({ exercises, setExercises, bodyPart }) {
-  console.log(exercises)
+  const [currentPage, setCurrentPage] = useState(1)
+  const exercisesPerPage = 9;
+
+
+  // Grabbing the exercises page
+  const indexOfLastExercise = currentPage * exercisesPerPage
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+  const paginate = (e,value) => {
+    setCurrentPage(value);
+    window.scrollTo({top: 1800, behavior: 'smooth'})
+  }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = []
+      if(bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', ExerciseOptions)
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, ExerciseOptions)
+      }
+      setExercises(exercisesData)
+    }
+    fetchExercisesData()
+  }, [bodyPart])
  
 
 
@@ -21,9 +45,22 @@ function Exercises({ exercises, setExercises, bodyPart }) {
         flexWrap="wrap"
         justifyContent="center"
       >
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
          <ExerciseCard key={index} exercise={exercise}/>
         ))}
+      </Stack>
+      <Stack mt='100px' alignItems='center'>
+          {exercises.length > 9 && (
+            <Pagination 
+              color='standard'
+              shape='rounded'
+              defaultPage={1}
+              count={Math.ceil(exercises.length / exercisesPerPage)}
+              page={currentPage}
+              onChange={paginate}
+              size='large'
+            />
+          )}
       </Stack>
     </Box>
   );  
