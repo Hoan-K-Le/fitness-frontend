13e45caa7844862411c1a61937faf08acb0bd711
Hoan <hoanleecr@gmail.com>
2023-03-15 14:13:50 -0700

    finish search and filtering out the exercise

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -8,7 +8,7 @@ function HeroBanner() {
         mt: {lg: '212px', xs: '70px'},
         ml: {sm: '50px'}
     }} position= 'relative' p='20px'>
-        <Typography color='#FF2625' fontweight="600" fontSize="26px">
+        <Typography color='#FF2625' fontWeight="600" fontSize="26px">
             Fitness Club
         </Typography>
         <Typography mb='23px' mt='30px' fontWeight={700} sx={{fontSize: {lg: '44px', xs: '40px'}}}>
@@ -17,7 +17,7 @@ function HeroBanner() {
         <Typography mb={4} fontSize='22px'  lineHeight='35px'>
             Check out the most effective exercises
         </Typography>
-        <Button variant='contained' color='error' href='#exercises'>Explore Exercises</Button>
+        <Button variant='contained' sx={{backgroundColor: '#ff2625', padding: '19px'}} color='error' href='#exercises'>Explore Exercises</Button>
         <Typography fontWeight={600} color='#ff2625' sx={{opacity: 0.1, diplsay: {log:'block', xs:'none'}}} fontSize='200px'>
             Exercise
         </Typography>
--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,8 +1,94 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import {Box, Button, Stack, TextField, Typography} from '@mui/material'
+
+import {ExerciseOptions, fetchData} from '../utils/fetchData'
+import HorizontalScrollbar from './HorizontalScrollbar'
 
 function SearchExercises() {
+  const [search, setSearch] = useState('')
+  const [exercises, setExercises] = useState([])
+  const [bodyParts, setBodyParts] = useState([])
+
+  // fetch the categories right when the page loads
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', ExerciseOptions);
+      setBodyParts(['all', ...bodyPartsData])
+    }
+    fetchExercisesData()
+
+  },[])
+
+
+
+
+const handleSearch = async () => {
+  // if search exists then fetch data
+  if(search) {
+      const exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', ExerciseOptions)
+      
+      // Filter the exercise we're searching
+      const searchedExercises = exerciseData.filter(exercise => {
+        // Able to search up not just name, but for equipment, what it targets, and the bodypart its for
+        exercise.name.toLowerCase().includes(search)
+        || exercise.equipment.toLowerCase().includes(search)
+        || exercise.target.toLowerCase().includes(search)
+        || exercise.bodyPart.toLowerCase().includes(search)
+      })
+      
+      setSearch('')
+      setExercises(searchedExercises)
+  }
+}
+
+
+
+
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems='Center' mt='37px' justifyContent='center' p='20px'>
+      <Typography fontWeight={700} sx={{fontSize:{lg: '44px', xs:'30px'}}} mb='50px' textAlign='center'>
+        Awesome Exercises You <br/> Should know
+      </Typography>
+
+      <Box position='relative' mb='72px'>
+      <TextField
+      sx={{
+        input: {fontWeight:'700', 
+        border: 'none', 
+        borderRadius: '4px'},
+        width: {lg: '800px', xs:'350px'},
+        backgroundColor: '#fff',
+        borderRadius: '40px'
+
+      }}
+      height='76px'
+      value={search}
+      onChange={(e) => setSearch(e.target.value.toLowerCase())}
+      placeholder='Search'
+      type='text'
+      >
+      </TextField>
+
+      <Button className='search-btn'
+      sx={{bgcolor: '#FF2625',
+        color: '#fff',
+        textTransform: 'none',
+        width: {lg: '175px', xs:'80px'},
+        fontSize: {lg: '20px', xs: '14px'},
+        height: '56px',
+        position: 'absolute',
+        right: '0'
+    }}
+    onClick={handleSearch}
+      >
+        Search
+      </Button>
+      </Box>
+
+      <Box sx={{position: 'relative', width: '100%', p: '20px'}}>
+        <HorizontalScrollbar data={bodyParts}/>
+      </Box>
+    </Stack>
   )
 }
 
