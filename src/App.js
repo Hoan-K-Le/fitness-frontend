777d66fd8abbfb077fadfabb4cb52a619cc02b04
Hoan <hoanleecr@gmail.com>
2023-03-15 03:13:03 -0700

    finish setting up navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
 import './App.css';
+import React from 'react'
 // To route between our pages
 import {Route, Routes} from 'react-router-dom'
+// Helps with shading
 import {Box} from '@mui/material'
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      APP
-    </div>
+    // setting the width for the big screen and margin to auto;
+   <Box width='400px' sx={{width: {xl: '1488px'}}} m='auto'>
+    <NavBar/>
+    <Routes>
+      {/* The path of a website, element ={which component file we using} */}
+      <Route path='/' element={<Home/>}/>
+      <Route path='/exercise/:id' element={<ExerciseDetail />}/>
+    </Routes>
+    <Footer/>
+   </Box>
   );
 }
 
