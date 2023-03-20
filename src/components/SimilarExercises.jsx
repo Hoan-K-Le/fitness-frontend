807859906f4fdae3476c96ff2824febfc29199f5
Hoan <hoanleecr@gmail.com>
2023-03-20 12:56:23 -0700

    finish similiar exercises and footer

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import {Box, Stack, Typography} from '@mui/material'
+import HorizontalScrollbar from './HorizontalScrollbar'
+import Loader from './Loader'
+
+
 
 function SimilarExercises({targetMuscleExercises, equipmentExercises}) {
   return (
     <Box sx={{mt: {lg: '100px', sx: '0'}}}>
-        <Typography>Exercises that target the same muscle group</Typography>
+        <Typography variant='h3' mb={5}>Exercises that target the same muscle group</Typography>
+        <Stack direction='row' sx={{p: '2', position: 'relative'}}>
+            {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises}/>
+            : <Loader/>
+        }
+        </Stack>
+        <Typography variant='h3' mb={5}>Exercises that target the same equipment</Typography>
+        <Stack direction='row' sx={{p: '2', position: 'relative'}}>
+            {equipmentExercises.length ? <HorizontalScrollbar data={equipmentExercises}/>
+            : <Loader/>
+        }
+        </Stack>
     </Box>
   )
 }
